c0d4326f264fdf8cd2dbc9e8cde14b29723c59d4
Allyson Monteiro <allysonfield2@gmail.com>
2021-12-05 14:29:00 -0300

    Eslint e Prettier configurados para Typescript

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import {useNavigation} from '@react-navigation/core';
 import {Text, TouchableOpacity} from 'react-native';
 
+import {StackNavigationProp} from '@react-navigation/stack';
 import {Container} from './styled';
+import {RootStackParamList} from '~/routes/routes';
 
-const Home = ({navigation}) => {
+type HomeScreenProp = StackNavigationProp<RootStackParamList>;
+
+const Home = () => {
+  const {navigate} = useNavigation<HomeScreenProp>();
   return (
     <Container>
       <Text>Home</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+      <TouchableOpacity onPress={() => navigate('Details')}>
         <Text>Go to details</Text>
       </TouchableOpacity>
     </Container>
