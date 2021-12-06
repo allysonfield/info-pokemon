d899d74cb7b82558f15c4fa9dbbcc4f32a95a398
Allyson Monteiro <allysonfield2@gmail.com>
2021-12-06 18:31:44 -0300

    Tela de detalhes dos pokemons concluída

--- src/@types/interfaces.ts
+++ src/@types/interfaces.ts
@@ -1,5 +1,39 @@
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '~/routes/routes';
+
 export interface DataPokeList {
   name: string;
   url: string;
 }
 [];
+
+export type navigationProp = StackNavigationProp<RootStackParamList>;
+
+type specie = {name: string};
+
+type sprite = {other: {home: {front_default: string}}};
+
+type ability = {ability: {name: string}}[];
+
+type form = {name: string};
+
+type data = {
+  species: specie;
+  sprites: sprite;
+  abilities: ability;
+  forms: form[];
+  height: number;
+  weight: number;
+};
+export interface PokemonProps {
+  data: data;
+  name: string;
+  url: string;
+}
+type Pokemon = {
+  pokemons: PokemonProps[];
+  loading: boolean;
+};
+export interface PokemonReducerProps {
+  pokemon: Pokemon;
+}
